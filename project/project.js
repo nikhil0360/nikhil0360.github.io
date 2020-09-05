b832c14ab1e637670efb288fd7957d73ad66d5ce
Nikhil Agarwal <nikhil.agarwal@iiitb.org>
2020-09-05 16:56:34 +0530

    make changes in grammar of Text using grammarly

--- project/project.js
+++ project/project.js
@@ -3,16 +3,16 @@ var defHTML = document.getElementsByClassName('view') ;
 var def = defHTML[0].innerHTML ;
 
 var tab1c =
-"<h1>Pybush 🌲</h1><img src='images/project1.png'><p>binarytree is a great module to implement trees in python. Since python doesn't support pointers; implementing binary tree functions was a bit tricky, but no more 😁. pybush brings this functionality using binarytree module. Now you can do everything from creating a tree, printing the tree, implement avl, do rotations and other functions.</p>" ;
+"<h1>Pybush 🌲</h1><img src='images/project1.png'><p>binarytree is a great module to implement trees in python. Since python doesn't support pointers; implementing binary tree functions was a bit tricky, but no more 😁. pybush brings this functionality using the binarytree module. Now you can do everything from creating a tree, printing the tree, implement avl, do rotations and other functions.</p>" ;
 
 var tab2c =
-"<h1>OpenCV: Face Detection, Image diff, Guesture gaming</h1><img src='images/project2.png' style='width:50vh'><p>OpenCV is a really powerful module and using it i created this class of project. </p><p>All of these projects are small and fun to implement, and you can learn a lot about image processing and comeup with different solutions and fun project</p> " ;
+"<h1>OpenCV: Face Detection, Image diff, Guesture gaming</h1><img src='images/project2.png' style='width:50vh'><p>OpenCV is a really powerful module and using it I created these classes of project. </p><p>All of these projects are small and fun to implement, and you can learn a lot about image processing and come up with different solutions and fun project</p>";
 
 var tab3c =
-"<h1>Snake Game</h1><img src='images/project3.png' style='width:40vh'><p>Pygame, a very beginner friendly module was used to make this project. This game is a colorful version of the classic snake game. I really worked a great idea on the color of the snake to make it more of my type.</p> " ;
+"<h1>Snake Game</h1><img src='images/project3.png' style='width:40vh'>Pygame, a very beginner-friendly module, was used to make this project. This game is a colorful version of the classic snake game. I worked a great idea on the color of the snake to make it more of my type.</p> " ;
 
 var tab4c =
-"<h1>PIL-Tesseract-OpenCV</h1> <p>It is a great which does the following, takes a newspaper cliping and find faces on that image if a particular keyword is found in the text of the newspaper. It uses the module PIL, Pytesseract and OpenCV. This was does as a part of Online courses on Coursera</p><img src='images/project4.png' style='width:70vh'>" ;
+"<h1>PIL-Tesseract-OpenCV</h1> <p>It is a great project which does the following, takes a newspaper clipping and finds faces on that image if a particular keyword is found in the text of the newspaper. It uses module PIL, Pytesseract, and OpenCV. This was done as a part of Online courses on Coursera</p><img src='images/project4.png' style='width:70vh'>" ;
 
 
 var view = document.getElementsByClassName('view') ;
